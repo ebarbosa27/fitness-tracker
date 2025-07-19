8c6565777f6323caf9ec25c698e0aaf322d9fa39
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-19 12:15:53 -0500

    developed activity page to use the useQuery function to call for activities

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import useQuery from "../api/useQuery";
+
 export default function ActivitiesPage() {
+  const { data, loading, error } = useQuery("/activities", "allActivities");
+
+  if (error) {
+    return (
+      <>
+        <h1>Error fetching activities</h1>
+      </>
+    );
+  }
+
+  if (loading) {
+    return (
+      <>
+        <h1>Loading activities...</h1>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>Activities</h1>
-      <p>Imagine all the activities!</p>
+      {data ? (
+        <ul>
+          {data.map((activity, idx) => (
+            <li key={idx}>{activity.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Imagine all the activities!</p>
+      )}
     </>
   );
 }
