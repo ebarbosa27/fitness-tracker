68263ecfae5634d111cfae7f391ba0fc0e165872
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-20 22:41:24 -0500

    added error message to new activity when it fails

--- src/activities/AddActivity.jsx
+++ src/activities/AddActivity.jsx
@@ -3,20 +3,24 @@ import useMutation from "../api/useMutation";
 export default function AddActivity() {
   const addReq = useMutation("POST", "/activities", ["allActivities"]);
 
-  function handleAddActivity(e) {
-    e.preventDefault();
-    const formData = new FormData(e.target);
+  function handleAddActivity(evt) {
+    evt.preventDefault();
+    const formData = new FormData(evt.target);
     const newActivity = { name: formData.get("name"), description: formData.get("description") };
-
     addReq.mutate(newActivity);
+  }
+
+  if (addReq.loading) {
+    return <div>Loading . . .</div>;
   }
 
   return (
     <form onSubmit={handleAddActivity}>
-      <p>Add new activity</p>
+      <h2>Add new activity</h2>
       <input type="text" placeholder="Name" name="name" />
       <input type="text" placeholder="Description" name="description" />
       <input type="submit" />
+      {addReq.error ? <p className="errorMessage">{addReq.error}</p> : ""}
     </form>
   );
 }
